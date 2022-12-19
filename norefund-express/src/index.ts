9aca6cf3ae5319b61989282320c757208767774a
ehdwoKIM <kinbell19@gmail.com>
2022-12-19 17:56:22 +0900

    초기 세팅

--- norefund-express/src/index.ts
+++ norefund-express/src/index.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import router from "./router";
 
 const app = express(); // express 객체 받아옴
-const PORT = 3000; // 사용할 port를 3000번으로 설정
+const PORT = 5000; // 사용할 port를 3000번으로 설정
 
 app.use(express.json()); // express 에서 request body를 json 으로 받아오겠다.
 
