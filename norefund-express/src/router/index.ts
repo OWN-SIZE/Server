b3fc4501963d01c02b995029eea1e3a1b8c14c4f
ehdwoKIM <kinbell19@gmail.com>
2022-12-20 17:13:53 +0900

    "초기 세팅"

--- norefund-express/src/router/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import userRouter from "./userRouter";
-
-const router: Router = Router();
-
-router.use("/user", userRouter);
-
-export default router;
--- norefund-express/src/router/userRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { userController } from "../controller";
-
-const router: Router = Router();
-
-router.get("/:userId", userController.getUserById);
-
-export default router;
