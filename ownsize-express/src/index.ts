2bc0cf8e6b61e2d164d66b533791c2d96f6e33f9
ehdwoKIM <kinbell19@gmail.com>
2022-12-20 17:26:58 +0900

    초기 세팅

--- ownsize-express/src/index.ts
+++ ownsize-express/src/index.ts
@@ -6,7 +6,7 @@ const PORT = 3001; // 사용할 port를 3000번으로 설정
 
 app.use(express.json()); // express 에서 request body를 json 으로 받아오겠다.
 
-//app.use("/api", require("./api")); // use -> 모든 요청
+app.use("/api", require("./api")); // use -> 모든 요청
 // localhost:8000/api -> api 폴더
 // localhost:8000/api/user -> user.ts
 
