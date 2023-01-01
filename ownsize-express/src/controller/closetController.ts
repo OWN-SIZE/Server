9ba039209f132ae797c73990f00d118c8e5aba68
ehdwoKIM <kinbell19@gmail.com>
2023-01-02 03:01:56 +0900

    [ADD] 전체 옷장 의류 정보 수정

--- ownsize-express/src/controller/closetController.ts
+++ ownsize-express/src/controller/closetController.ts
@@ -1,15 +1,36 @@
 import { Request, Response } from "express";
 import { closetService }  from "../service";
+import { rm, sc } from "../constants";
+import { success, fail } from "../constants/response";
 
 //* 전체 옷장 조회
 const getAllCloset = async (req: Request, res: Response) => {
   const data = await closetService.getAllCloset();
 
-  return res.status(200).json({ status: 200, message: "전체 옷장 조회 조회 성공", data });
-};
+  return res.status(sc.OK).send(success(sc.OK, rm.READ_ALLCLOSET_SUCCESS, data));
+}
+
+//* 전체 옷장 정보 수정
+const updateCloset = async (req: Request, res: Response) => {
+  const { productName, size, memo, isPin } = req.body;
+  const { productId } = req.params;
+
+  if (!productId || (!productName && !size && !memo && !isPin)) {
+    return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.ALLCLOSET_INFO_ERROR));
+  }
+
+  const data = await closetService.updateCloset(+productId, productName, size, memo, isPin);
+
+  if (!data) {
+    return res.status(sc.NOT_FOUND).send(fail(sc.NOT_FOUND, rm.UPDATE_ALLCLOSET_FAIL));
+  }
+  
+  return res.status(sc.CREATED).send(success(sc.CREATED, rm.UPDATE_ALLCLOSET_SUCCESS, data));
+}
 
 const closetController = {
   getAllCloset,
+  updateCloset
 };
 
 export default closetController;
--- ownsize-express/src/service/closetService.ts
+++ ownsize-express/src/service/closetService.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { serialize } from "v8";
 const prisma = new PrismaClient();
 
 //* 전체 옷장 조회
@@ -8,8 +9,26 @@ const getAllCloset = async () => {
     return data;
 };
 
+//* 전체 옷장 정보 수정
+const updateCloset = async (productId: number, productName?: string, size?: string, memo?: string, isPin?: boolean) => {
+    const data = await prisma.allCloset.update({
+        where: {
+            id: productId
+        },
+        data: {
+            productName: productName,
+            size: size,
+            memo: memo,
+            isPin: isPin
+        }
+    });
+
+    return data;
+}
+
 const closetService = {
     getAllCloset,
+    updateCloset,
 };
 
 export default closetService;
