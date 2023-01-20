9205ef94de55db61127f51f833998572e70e5015
ehdwoKIM <kinbell19@gmail.com>
2023-01-20 19:02:30 +0900

    [FIX] 마이사이즈 입력 코드 수정

--- ownsize-express/src/service/authService.ts
+++ ownsize-express/src/service/authService.ts
@@ -29,24 +29,12 @@ const register = async (email: string, name: string) => {
     console.log("user error");
     return null;
   }
-  
   //유저 등록 시 해당 유저의 mysize 입력칸 생성
-  await prisma.mySize.create({
+  await prisma.mySize.create({ 
     data: {
       userId: user.id,
-      topLength: null,
-      shoulder: null,
-      chest: null,
-      isWidthOfTop: null,
-      bottomLength: null,
-      waist: null,
-      thigh: null,
-      rise: null,
-      hem: null,
-      isWidthOfBottom: null
     }
   })
-
   // 생성된 토큰과 userId를 리턴
   const data = {
     userId: user.id,
