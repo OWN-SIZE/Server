695c259e12cd946b80e4e506735cbc31f8161a20
Hayan Jo <59385635+yanh2@users.noreply.github.com>
2023-02-10 16:29:54 +0900

    [FIX] 기존 토큰 확인 코드 없애고 토큰 재발급 API 추가

--- ownsize-express/src/controller/authController.ts
+++ ownsize-express/src/controller/authController.ts
@@ -19,8 +19,24 @@ const register = async (req: Request, res: Response) => {
   return res.status(sc.OK).send(success(sc.OK, rm.SIGNIN_SUCCESS, data)); // 로그인 성공
 };
 
+//* 엑세스 토큰 재발급
+const newToken = async (req: Request, res: Response) => {
+  const { userId } = req.body;
+
+  const data = await authService.newToken(+userId);
+
+  if (!data) {
+    //엑세스 토큰 안만들어진 경우
+    return res
+      .status(sc.NOT_FOUND)
+      .send(fail(sc.NOT_FOUND, rm.EXPIRED_ALL_TOKEN)); //access, refresh 둘다 만료된 경우->재로그인
+  }
+  return res.status(sc.OK).send(success(sc.OK, rm.CREATE_TOKEN_SUCCESS, data)); // 엑세스 토큰 재발급 성공
+};
+
 const authController = {
   register,
+  newToken,
 };
 
 export default authController;
--- ownsize-express/src/service/authService.ts
+++ ownsize-express/src/service/authService.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import jwtHandler from "../middlewares/jwtHandler";
+import tokenType from "../constants/tokenType";
 const prisma = new PrismaClient();
 require("dotenv").config();
 
@@ -46,8 +48,48 @@ const register = async (email: string, name: string) => {
   return data;
 };
 
+//* 엑세스 토큰 재발급 (access, refresh 둘다 만료됐을때는 실패 응답 보냄)
+const newToken = async (userId: number) => {
+  // DB에 저장된 refresh 토큰 확인
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
+
+  if (!user) {
+    console.log("user error");
+    return null;
+  }
+  if (!user.token) {
+    console.log("user refresh token empty");
+    return null;
+  }
+
+  const decodedRefresh = jwtHandler.verify(user.token);
+  if (decodedRefresh === tokenType.TOKEN_EXPIRED) {
+    //재로그인 필요한 경우
+    return null;
+  } else {
+    //refresh token 유효하니까 access token 재발급
+    const newAccessToken = jwt.sign(
+      {
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+    const data = {
+      userId: user.id,
+      token: newAccessToken,
+    };
+    return data;
+  }
+};
+
 const authService = {
   register,
+  newToken,
 };
 
 export default authService;
